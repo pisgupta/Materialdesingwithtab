7dba6b21e93a3a3ac39fead39f70f3deaf6a9f0e
pisgupta@gmail.com <rap.is.gupta@gmail.com>
2015-12-15 17:03:47 +0530

    tab
    tab with custom action bar

---
+++ MaterialTabLayout/app/src/main/java/com/example/pankaj/materialtablayout/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.pankaj.materialtablayout;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            android.R.drawable.ic_menu_help,
+            android.R.drawable.ic_menu_edit,
+            android.R.drawable.ic_menu_add};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        //----Call this function for displaying image in tab
+        //setupTabIcons();
+    }
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+//-------------------code to diaply tab with text------------
+
+//        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+//        tabOne.setText("ONE");
+//        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, android.R.drawable.ic_menu_help, 0, 0);
+//        tabLayout.getTabAt(0).setCustomView(tabOne);
+//
+//        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+//        tabTwo.setText("TWO");
+//        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, android.R.drawable.ic_menu_edit, 0, 0);
+//        tabLayout.getTabAt(1).setCustomView(tabTwo);
+//
+//        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+//        tabThree.setText("THREE");
+//        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, android.R.drawable.ic_menu_add, 0, 0);
+//        tabLayout.getTabAt(2).setCustomView(tabThree);
+    }
+
+       private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new OneFragment(), "ONE");
+        adapter.addFrag(new TwoFragment(), "TWO");
+        adapter.addFrag(new ThreeFragment(), "THREE");
+        adapter.addFrag(new FoureFragment(), "FOUR");
+//        adapter.addFrag(new FiveFragment(), "FIVE");
+//        adapter.addFrag(new SixFragment(), "SIX");
+//        adapter.addFrag(new SevenFragment(), "SEVEN");
+//        adapter.addFrag(new EightFragment(), "EIGHT");
+//        adapter.addFrag(new NineFragment(), "NINE");
+//        adapter.addFrag(new TenFragment(), "TEN");
+        viewPager.setAdapter(adapter);
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
